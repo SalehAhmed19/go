5338e0acf5d451bd3478f02d52f4768375c5fcfd
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-11-09 14:39:22 +0600

    array and slices

--- Basic/9_slices/slices.go
+++ Basic/9_slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	// slices are dynamic arrays - most used data structure in Go + useful methods
@@ -9,6 +12,7 @@ func main() {
 
 	fmt.Println(nums)
 
+	// make function creates slices, maps and channels -> make(type, length, capacity)
 	var numbers = make([]int, 2) // create slice of int with initial length 5 | third param is capacity (optional)
 	numbers[0]=10
 
@@ -24,6 +28,36 @@ func main() {
 	numberSlice:=[]int{10,20,30,40,50} // slice literal
 	fmt.Println(numberSlice)
 
+	// copy function copies elements from source slice to destination slice
+	var distSlice = make([]int, 0, 5)
+	distSlice = append(distSlice, 1,2,3,4,5)
+	var copyDist = make([]int, len(distSlice))
 
 
+	// copy(destination, source)
+	copy(copyDist, distSlice)
+
+	fmt.Println("disSlice: ", distSlice, "copySlice: ", copyDist)
+
+	// slice operators
+	numberList:=[]int{1,2,3}
+
+	fmt.Println(numberList[0:2]) // slicing from index 0 to 2 (excluding 2)
+	fmt.Println(numberList[:2]) // from start to index 2
+	fmt.Println(numberList[1:]) // from index 1 to end
+	fmt.Println(numberList[:]) // entire slice
+
+	// slice package
+	numsList1:=[]int{5,3,8,1,2}
+	numsList2:=[]int{10,20,30,40,50}
+
+	fmt.Println(slices.Equal(numsList1, numsList2)) // check equality
+
+	// 2D slices
+	numbs:=[][]int{
+		{1,2,3},
+		{4,5,6},
+	}
+
+	fmt.Println(numbs)
 }
